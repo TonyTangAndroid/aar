a6f569a628ef1528d7526aecb274a418ff9665c9
ztang <tonythompsoncmu@gmail.com>
2019-01-05 19:53:01 -0500

    Add tester to validate jar, aar and test library.

---
+++ normal/src/test/java/io/github/android/tang/tony/testdemo/FileUtilTest.java
@@ -0,0 +1,17 @@
+package io.github.android.tang.tony.testdemo;
+
+import com.google.common.truth.Truth;
+
+import org.junit.Test;
+
+import io.github.android.tang.tony.test.util.TestUtil;
+
+public class FileUtilTest {
+
+    @Test
+    public void content() {
+
+        Truth.assertThat(TestUtil.content("2.txt", this)).isEqualTo("{\"key\": 1}");
+
+    }
+}
